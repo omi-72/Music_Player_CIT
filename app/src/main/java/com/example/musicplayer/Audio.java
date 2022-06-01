577ce337a0f8a9e1e493fd3e19a2ab2e1ced4b64
omi-72 <jannat.omi722@gmail.com>
2022-06-01 21:48:32 +0600

    generate constuctor as String name & Uri audioUri on Audio class

--- app/src/main/java/com/example/musicplayer/Audio.java
+++ app/src/main/java/com/example/musicplayer/Audio.java
@@ -1,4 +1,14 @@
 package com.example.musicplayer;
 
+import android.net.Uri;
+
 public class Audio {
+
+    String name;
+    Uri audioUri;
+
+    public Audio(String name, Uri audioUri) {
+        this.name = name;
+        this.audioUri = audioUri;
+    }
 }
